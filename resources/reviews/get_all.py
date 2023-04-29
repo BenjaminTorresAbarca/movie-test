d01fd9a6c670ebcd2645b1da66b26dbdc48af547
Benjamin Torres <benjamintoab10@gmail.com>
2023-04-28 18:18:05 -0600

    Adding list review end point

---
+++ resources/reviews/get_all.py
@@ -0,0 +1,19 @@
+from common.auth import auth
+from resources.resource_base import ResourceBase
+from movie_driver_db.adapter.reviews import ReviewsAdapter
+
+
+class GetAllMoviesAPI(ResourceBase):
+    decorators = [auth.login_required]
+
+    def __init__(self):
+        super(GetAllMoviesAPI, self).__init__()
+
+    def get(self, movie_id):
+        try:
+            functions = ReviewsAdapter()
+            rows = functions.list(movie_id)
+            return rows
+
+        except Exception as e:
+            return self.handle_error(e)
